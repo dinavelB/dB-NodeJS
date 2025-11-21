837c42e31a316e1ff8369ed22cdca735c495c8d5
Dinavel Binongo <binongo.dinavel.escodero@gmail.com>
2025-11-22 00:09:43 +0800

    student gradebook v.1

--- Api-test/studentgradebook.js
+++ Api-test/studentgradebook.js
@@ -4,21 +4,24 @@ const readline = require('readline');
 
 const pathfile = pathFile.join(__dirname, '../Documentation/recordstudent/students.txt')
 
+//creates a variable for readline terminal interacivity
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
+//helper method, its async because we use callbacks here
 async function students(infos){
     return new Promise((resolve) => {
         rl.question(infos, answer => resolve(answer))
     })
 }
 
+//method that asks a questions then returns values
 async function start(){
-    const studentName = await students("Enter the student name");
-    const studentID = parseFloat(await students("Enter student ID"));
-    const studentCourse = await students("Enter student course");
+    const studentName = await students("Enter the student name: \n");
+    const studentID = parseFloat(await students("Enter student ID: \n"));
+    const studentCourse = await students("Enter student course: \n");
 
     return ({studentName, studentID, studentCourse})
 }
@@ -28,6 +31,7 @@ class Student{
     studentID;
     studentCourse;
 
+    //destructurig here, it will turn into instance
     constructor({studentName, studentID, studentCourse}){
         this.studentName = studentName;
         this.studentID = studentID;
@@ -38,16 +42,25 @@ class Student{
 class Records{
 
     constructor(){
-        this.records =   []
+        this.records =   [] //array
     }
 
     addStudent(student){
         if(typeof student !== 'object') throw "Error";
 
+        this.records.push(student)
+
         return new Promise((resolve, reject) =>{
+            //asynchronous callbacks needs promise
+            console.log("======Student Name============Student ID============Student Course======")
+            const lineStudents = //this is spacing on the and of each responses
+            `${student.studentName.padEnd(30)}` +
+            `${String(student.studentID).padEnd(22)}` +
+            `${student.studentCourse}\n`;
 
-            const lineStudents = `${student.studentName} ${student.studentID}, ${student.studentCourse}\n`
 
+            //promise, returns a value. only reject or resolve
+            //value can be use to other methods or storing
             filesystem.appendFile(pathfile, lineStudents, 'utf8', (error) =>{
                 if (error) return reject(error) //practice to return a resoslve or reject
 
@@ -59,3 +72,16 @@ class Records{
         })
     }
 }
+
+//dont forget to call a method when you wrap it :)
+async function main(){
+    const studentInput = await start();
+    const students = new Student(studentInput)
+    const bookrecords = new Records()
+    
+
+    const fileSystem = await bookrecords.addStudent(students) //addStudent do return a value, its from the file systems
+    console.log(fileSystem)
+}
+
+main()
